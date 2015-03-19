13d76da3aed95b30bf89080daaad6d131b7bd03c
Tom Dyer <tdyer1@gmail.com>
2015-03-19 02:47:44 -0400

    fix test to get a non-existant movie

--- spec/requests/movies_errors_spec.rb
+++ spec/requests/movies_errors_spec.rb
@@ -4,9 +4,11 @@ describe 'Movies' do
 
   before do
     @movie = Movie.create!(title: "Birdman", rating: 'r', total_gross: 9_300_000, description: "birdman", released_on: Date.parse("October 17, 2014"))
+  end
 
+  before(:all) do
     # DO NOT Raise exceptions
-    Wdi6RailsApiMovies::Application.config.action_dispatch.show_exceptions = true
+    # Wdi6RailsApiMovies::Application.config.action_dispatch.show_exceptions = true
     # Rails.application.configure{config.action_dispatch.show_exceptions = true }
     # Rails.application.configure{puts "Show Exceptions: " + config.action_dispatch.show_exceptions.to_s }
   end
@@ -23,14 +25,13 @@ describe 'Movies' do
     # expect(response).to be_error # status is 500
     expect(response.status).to eq 404
 
-    # "{\"error\":\"internal-server-error\",\"exception\":\"ActiveRecord::RecordNotFound : Couldn't find Movie with 'id'=9999\"}"
     movie_error = JSON.parse(response.body)
 
-    expect(movie_error['error']).to eq 'not_found'
-    expect(movie_error['status']).to eq 404
-    expect(movie_error['original_path']).to eq '/movies/9999'
+    expect(movie_error['railsErrorSymbol']).to eq 'not_found'
+    expect(movie_error['httpStatus']).to eq 404
+    expect(movie_error['originalPath']).to eq '/movies/9999'
 
-    expect(movie_error['exception']).to match(/ActiveRecord::RecordNotFound.*find Movie with 'id'=9999/)
+    expect(movie_error['detail']).to match(/ActiveRecord::RecordNotFound.*find Movie with 'id'=9999/)
 
   end
 
